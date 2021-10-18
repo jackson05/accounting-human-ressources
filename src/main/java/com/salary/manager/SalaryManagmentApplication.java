4a3491e8193ecce792ebae273d95e5553c5b57ea
jackson5g.bi@gmail.com <jackson5g.bi@gmail.com>
2021-10-18 11:31:50 +0100

    initial setup

---
+++ src/main/java/com/salary/manager/SalaryManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.salary.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salary/manager/SalaryManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salary.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
