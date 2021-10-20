1b6a1bdf535dfa039753afaac52198b90d3408fd
jackson5g.bi@gmail.com <jackson5g.bi@gmail.com>
2021-10-20 14:06:01 +0100

    property definition for authentication when requested

--- src/main/java/com/salary/manager/security/jwt/model/AuthenticationRequest.java
+++ src/main/java/com/salary/manager/security/jwt/model/AuthenticationRequest.java
@@ -1,5 +1,31 @@
 package com.salary.manager.security.jwt.model;
 
 public class AuthenticationRequest {
+	private String username;
+	private String password;
+	
+	public AuthenticationRequest() {
+		
+	}
+	
+	public AuthenticationRequest(String username,String password) {
+		
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
 
 }
