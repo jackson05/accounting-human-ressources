386b1d75cb8d244b8bbd2dce2498f615fde7ed0d
jackson5g.bi@gmail.com <jackson5g.bi@gmail.com>
2021-10-20 15:11:24 +0100

    definition of attributes for Authentication response

--- src/main/java/com/salary/manager/security/jwt/model/AuthenticationResponse.java
+++ src/main/java/com/salary/manager/security/jwt/model/AuthenticationResponse.java
@@ -2,4 +2,13 @@ package com.salary.manager.security.jwt.model;
 
 public class AuthenticationResponse {
 
+	private final String jwt;
+	
+	public AuthenticationResponse(String jwt) {
+		this.jwt=jwt;
+	}
+	
+	public String getJwt() {
+		return jwt;
+	}
 }
