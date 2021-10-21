0dee7da0e30e8f8d8b1de0e7616c6f3d6661b8e0
jackson5g.bi@gmail.com <jackson5g.bi@gmail.com>
2021-10-21 19:45:48 +0100

    ajout des beans

--- src/main/java/com/salary/manager/security/conf/SecurityConfigurer.java
+++ src/main/java/com/salary/manager/security/conf/SecurityConfigurer.java
@@ -2,8 +2,10 @@ package com.salary.manager.security.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
