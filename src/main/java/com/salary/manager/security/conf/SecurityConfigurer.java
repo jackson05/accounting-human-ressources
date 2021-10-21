cdcf3d6fa28d4462b286e2d31cd345d10d98d6e2
jackson5g.bi@gmail.com <jackson5g.bi@gmail.com>
2021-10-21 19:29:37 +0100

    security configuration package

---
+++ src/main/java/com/salary/manager/security/conf/SecurityConfigurer.java
@@ -0,0 +1,9 @@
+package com.salary.manager.security.conf;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
+	
+	
+
+}
