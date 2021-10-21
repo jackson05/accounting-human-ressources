da7a50588435141d4b4a9a38461ab1d902d4cb2b
jackson5g.bi@gmail.com <jackson5g.bi@gmail.com>
2021-10-21 19:41:25 +0100

    configuration d'un systeme de gestion de l'authentication base des detailles de l'utilisateur

--- src/main/java/com/salary/manager/security/conf/SecurityConfigurer.java
+++ src/main/java/com/salary/manager/security/conf/SecurityConfigurer.java
@@ -1,9 +1,18 @@
 package com.salary.manager.security.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private UserDetailsServices userDetailsServices;
 	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		auth.userDetailsService(userDetailsServices);
+	}
 
 }
