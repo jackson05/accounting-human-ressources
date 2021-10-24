7aafe03f7f5ab662f7bb23fd1666849697f103cb
jackson05 <jackson5g.bi@gmail.com>
2021-10-24 15:51:37 +0200

    banning and authorization of some url

--- src/main/java/com/salary/manager/security/conf/SecurityConfigurer.java
+++ src/main/java/com/salary/manager/security/conf/SecurityConfigurer.java
@@ -2,6 +2,7 @@ package com.salary.manager.security.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -15,6 +16,17 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
 		auth.userDetailsService(userDetailsServices);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http
+			.authorizeRequests()
+				.antMatchers("/authenticate")
+				.permitAll()
+				.anyRequest()
+				.authenticated();
 	}
 
 }
