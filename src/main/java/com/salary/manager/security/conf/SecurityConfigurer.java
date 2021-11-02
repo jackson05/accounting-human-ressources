6e255eb604a9e362664d55340b8e2ba41ead9061
jackson05 <jackson5g.bi@gmail.com>
2021-11-02 19:19:04 +0200

    bean definition for authentication manager and style of passwordEncoder

--- src/main/java/com/salary/manager/security/conf/SecurityConfigurer.java
+++ src/main/java/com/salary/manager/security/conf/SecurityConfigurer.java
@@ -1,10 +1,14 @@
 package com.salary.manager.security.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
@@ -26,7 +30,20 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
 				.antMatchers("/authenticate")
 				.permitAll()
 				.anyRequest()
-				.authenticated();
+				.authenticated();	
+	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		
+		return super.authenticationManagerBean();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
