426c30526b76e70e5410e00d77918f634e228791
carmel26 <carmel@ibi-africa.com>
2021-11-08 23:19:27 +0200

    finalisation de travail sur la mise a jour manuel avec un simple requette du 8=11-2021 a 23h19

--- src/main/java/com/salary/manager/departement/DepartementController.java
+++ src/main/java/com/salary/manager/departement/DepartementController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+ 
 
 @RestController
 public class DepartementController {
@@ -28,6 +29,10 @@ public class DepartementController {
 		return departementsService.getAllDepartement();
 	}
 	
+	@RequestMapping(value = "/departement/test/{id}", method = RequestMethod.GET)
+	public List<Departement> listdepartementTest(@PathVariable int id){
+		return departementsService.getDep(id);
+	}
 	@RequestMapping(value = "/departement/name/{designationDepartment}", method = RequestMethod.GET) 
 	public Departement departementByName(@PathVariable String designationDepartment) {
 		return departementsService.getByDesignationDepartement(designationDepartment);
@@ -39,9 +44,14 @@ public class DepartementController {
 	}
 	
 	@PutMapping("/departement/{id}")
-	public Departement updatedepartement(@PathVariable int id,@RequestBody Departement departement) {
-		return departementsService.updateDepartement(id, departement);
+	public Departement updatedepartement(@PathVariable int id,@RequestBody Departement des) {
+		return departementsService.updateDepartement(id, des);
 	}
+	
+//	@PutMapping("/departement/{id}")
+//	public Departement updatedepartement(@PathVariable int id,@RequestBody Departement des) {
+//		return departementsService.miseAjour(id, des);
+//	}
 	
 	@DeleteMapping("/departement/{id}")
 	public void deletedepartement(@PathVariable int id) {
--- src/main/java/com/salary/manager/departement/DepartementService.java
+++ src/main/java/com/salary/manager/departement/DepartementService.java
@@ -2,9 +2,15 @@ package com.salary.manager.departement;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * @author nkeshicar
+ *
+ */
 @Service
 public class DepartementService {
  
@@ -39,6 +45,36 @@ public class DepartementService {
     
     public void deleteDepartement (int id) {
     	departementsRepository.deleteById(id);
+    }
+    
+    /*
+     * custom query of update
+     * 1er etapes creation de l'objet EntityManager
+    */
+    EntityManager ent;
+    public Departement customUpdateDepartement(Departement departement) {
+    	
+    	ent.createNativeQuery("UPDATE departement SET date_created_departement=?1,date_modified_departement=?2,"
+    			+ "designation_department=?3,user_created_departement=?4,user_modified_departement=?5 WHERE id=?6")
+    		.setParameter(1, departement.getDateCreatedDepartement())
+    		.setParameter(2, departement.getDateModifiedDepartement())
+    		.setParameter(3, departement.getDesignationDepartment())
+    		.setParameter(4, departement.getUserCreatedDepartement())
+    		.setParameter(5, departement.getUserModifiedDepartement())
+    		.setParameter(6, departement.getId())
+    		.executeUpdate();
+    	 
+    	return departement;
+    }
+    
+    public Departement miseAjour(int id, Departement des) {
+    	System.out.println("--"+des.getDesignationDepartment());
+    	departementsRepository.updateDepartementById(des.getDesignationDepartment(), id);
+    	return des;
+    }
+    
+    public List<Departement> getDep(int id) { 
+    	return departementsRepository.selectDepId(id);
     }
 }
 
