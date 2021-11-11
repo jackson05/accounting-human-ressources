0a3db2ca710d7ccf2477061236294601fca61be6
jackson RUGAMBA <jackson5g.bi@gmail.com>
2021-11-11 11:38:03 +0200

    password Encryption added

--- src/main/java/com/salary/manager/security/conf/SecurityConfigurer.java
+++ src/main/java/com/salary/manager/security/conf/SecurityConfigurer.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -52,9 +52,11 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
 	}
 	
 	@Bean
-	public PasswordEncoder passwordEncoder()
-	{
-		return NoOpPasswordEncoder.getInstance();
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
+
+	
+
 
 }
