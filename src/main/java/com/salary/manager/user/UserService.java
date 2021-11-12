51a3f85fa0ba72416f5ebe6e0374ce2df860c779
jackson RUGAMBA <jackson5g.bi@gmail.com>
2021-11-12 16:21:48 +0200

    implementation d'une fonction qui instancie les details d'un utilisateur

--- src/main/java/com/salary/manager/user/UserService.java
+++ src/main/java/com/salary/manager/user/UserService.java
@@ -1,14 +1,27 @@
 package com.salary.manager.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
 
-public class UserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private UserRepository userRepo;
 	
 	public User create(User user) {
 		return userRepo.save(user);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		
+		 User user=userRepo.findByUserName(userName);
+		 return new MyUserDetails(user);
 	}
 	
 
