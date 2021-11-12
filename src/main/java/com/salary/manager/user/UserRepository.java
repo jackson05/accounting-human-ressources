76f67926f8fd1722649bb78602dd2022f01596d5
jackson RUGAMBA <jackson5g.bi@gmail.com>
2021-11-12 16:20:36 +0200

    methode qui recupere user par son nom d'utilisateur

--- src/main/java/com/salary/manager/user/UserRepository.java
+++ src/main/java/com/salary/manager/user/UserRepository.java
@@ -1,9 +1,12 @@
 package com.salary.manager.user;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer>
 {
+	User findByUserName(String name);
 
 }
