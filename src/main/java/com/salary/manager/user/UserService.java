944a0ab09686fd11d65dfa406c851ff93ddbc100
jackson RUGAMBA <jackson5g.bi@gmail.com>
2021-11-12 14:28:42 +0200

    Initial method that can save user

--- src/main/java/com/salary/manager/user/UserService.java
+++ src/main/java/com/salary/manager/user/UserService.java
@@ -1,5 +1,15 @@
 package com.salary.manager.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserService {
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	public User create(User user) {
+		return userRepo.save(user);
+	}
+	
 
 }
