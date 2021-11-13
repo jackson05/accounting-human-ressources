9b7bd1053f80b8b37b445c8701100af1f0f634d5
jackson RUGAMBA <jackson5g.bi@gmail.com>
2021-11-13 21:58:36 +0200

    trying to create relation ManyToMany between user and Role

--- src/main/java/com/salary/manager/user/role/Role.java
+++ src/main/java/com/salary/manager/user/role/Role.java
@@ -3,24 +3,40 @@ package com.salary.manager.user.role;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import com.salary.manager.user.User;
 
 @Entity
 public class Role {
+	
 	@Id @GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String roleName;
 	private String createdDate;
 	private String modifiedDate;
 
-	@ManyToMany(mappedBy = "roles")
-	private List<User> user=new ArrayList<User>();
+	@ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+	@JoinTable(
+			name="user_roles",
+			joinColumns = @JoinColumn(
+					name="role_id",
+					referencedColumnName = "id"
+					),
+			inverseJoinColumns = @JoinColumn(
+					name="user_id",
+					referencedColumnName="id")
+			
+			)
+	private List<User> user=new ArrayList<>();
 
 	public String getRoleName() {
 		return roleName;
