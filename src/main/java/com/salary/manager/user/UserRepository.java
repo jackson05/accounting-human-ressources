9ca53a505b5a3a6dec11ad9fc2e7c891f031ef2b
jackson RUGAMBA <jackson5g.bi@gmail.com>
2021-11-13 20:31:50 +0200

    optional abstract User method can return user by username

--- src/main/java/com/salary/manager/user/UserRepository.java
+++ src/main/java/com/salary/manager/user/UserRepository.java
@@ -1,12 +1,14 @@
 package com.salary.manager.user;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer>
 {
-	User findByUserName(String name);
+	Optional<User> findByUserName(String name);
 
 }
