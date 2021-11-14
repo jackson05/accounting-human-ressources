53c3876c5dcebe31cf92c6fd243cf1b76c4c1a0f
jackson RUGAMBA <jackson5g.bi@gmail.com>
2021-11-14 12:32:45 +0200

    repository annotation added

--- src/main/java/com/salary/manager/user/role/RoleRepository.java
+++ src/main/java/com/salary/manager/user/role/RoleRepository.java
@@ -1,6 +1,8 @@
 package com.salary.manager.user.role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role,Integer>{
 
 }
