65527a95a807978134369820e66ab52655cd5f90
jackson RUGAMBA <jackson5g.bi@gmail.com>
2021-11-14 12:39:16 +0200

    Based method created

--- src/main/java/com/salary/manager/user/role/RoleService.java
+++ src/main/java/com/salary/manager/user/role/RoleService.java
@@ -1,6 +1,27 @@
 package com.salary.manager.user.role;
 
-public class RoleService {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RoleService {
 	
+	@Autowired
+	private RoleRepository roleRepo;
+	
+	public Role save(Role role) {		
+		return roleRepo.save(role);
+	}
+	
+	public Role delete(Role role) {
+		roleRepo.delete(role);
+		return role;
+	}
+	
+	public List<Role> findAllRoles(){
+		List<Role> roles=new ArrayList<>();
+		roleRepo.findAll().forEach(roles::add);
+		return roles;			
+	}
 }
