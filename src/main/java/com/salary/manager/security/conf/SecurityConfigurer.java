76b102b4420c9de75f05ba8bbbb007c1a818185b
jackson RUGAMBA <jackson5g.bi@gmail.com>
2021-11-14 20:32:12 +0200

    no change

--- src/main/java/com/salary/manager/security/conf/SecurityConfigurer.java
+++ src/main/java/com/salary/manager/security/conf/SecurityConfigurer.java
@@ -13,12 +13,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.salary.manager.security.jwt.util.JwtRequestFilter;
+import com.salary.manager.user.UserService;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
 	
+//	@Autowired
+//	private UserDetailsServices userDetailsServices;
+	
 	@Autowired
-	private UserDetailsServices userDetailsServices;
+	private UserService userDetailsServices;
 	
 	@Autowired
 	private JwtRequestFilter jwtRequestFilter;
@@ -32,7 +36,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
-		http
+		http.csrf().disable()
 			.authorizeRequests()
 				.antMatchers("/authenticate")
 				.permitAll()
@@ -47,7 +51,6 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
 	@Override
 	@Bean
 	public AuthenticationManager authenticationManagerBean() throws Exception {
-		
 		return super.authenticationManagerBean();
 	}
 	
