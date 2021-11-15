b040e3e6cdd3efa8c9220f49a7fe724ed0e58a1e
jackson RUGAMBA <jackson5g.bi@gmail.com>
2021-11-15 21:54:46 +0200

    add controller end point added

--- src/main/java/com/salary/manager/user/role/RoleController.java
+++ src/main/java/com/salary/manager/user/role/RoleController.java
@@ -1,6 +1,18 @@
 package com.salary.manager.user.role;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/role")
 public class RoleController {
+	
+	@Autowired
+	private RoleRepository roleRepo;
+	
+	public Role addRole(@RequestBody Role role) {
+		return roleRepo.save(role);
+	}
 	
 
 }
