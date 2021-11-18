3caba2e6492a64f6d579cf661977a1499be4cfdf
jackson05 <jackson5g.bi@gmail.com>
2021-11-18 22:36:47 +0200

    method renamed

--- src/main/java/com/salary/manager/user/UserRepository.java
+++ src/main/java/com/salary/manager/user/UserRepository.java
@@ -3,7 +3,7 @@ package com.salary.manager.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.salary.manager.test.model.User;
+//import com.salary.manager.test.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer>
