9f3b84a65c65a9f8797ae254145b6edaa9eaf094
jackson05 <jackson5g.bi@gmail.com>
2021-11-19 16:28:52 +0200

    optimisation du controlleur

--- src/main/java/com/salary/manager/user/UserController.java
+++ src/main/java/com/salary/manager/user/UserController.java
@@ -1,40 +1,28 @@
-//package com.salary.manager.user;
-//
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.BadCredentialsException;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.salary.manager.security.jwt.model.AuthenticationRequest;
-//import com.salary.manager.security.jwt.model.AuthenticationResponse;
-//import com.salary.manager.security.jwt.util.JwtUtil;
-//
-//@RestController
-//public class UserController {
-//	
+package com.salary.manager.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
 //	@Autowired
 //	private AuthenticationManager authenticationManager;
-//	
-//	@Autowired
-//	private UserService userDetailsService;
-//	
-//	@GetMapping("/hello")
-//	public String hello() {
-//		
-//		return "hello world";
-//	}
-//	
+	
+	@Autowired
+	private UserService userDetailsService;
+	
+	@GetMapping("/hello")
+	public String hello() {
+		
+		return "hello world";
+	}
+	
 //	@Autowired
 //	private JwtUtil jwtTokenUtil;
 //
@@ -61,34 +49,34 @@
 //		return ResponseEntity.ok(new AuthenticationResponse(jwt));
 //
 //	}
-//	
-//	
+	
+	
 //	@PostMapping("/user/add/{user}")
 //	public User addUser(@RequestBody User user) {
 //		return userDetailsService.save(user);
 //	}
-//	
-//	
-//	@GetMapping("/users/load")
-//	public List<User> getUsers(){
-//		return userDetailsService.loadUsers();
-//	}
-//	
-//	@DeleteMapping("/user/delete/{user}")
-//	public List<User> deleteUser(@RequestBody User user){
-//		 userDetailsService.delete(user);
-//		 return userDetailsService.loadUsers();
-//	}
-//	
-//	@DeleteMapping("/user/{id}/delete")
-//	public List<User> deleteUser(@RequestBody int id){
-//		userDetailsService.deleteById(id);
-//		return userDetailsService.loadUsers();
-//	}
-//	
+	
+	
+	@GetMapping("/users/load")
+	public List<User> getUsers(){
+		return userDetailsService.loadUsers();
+	}
+	
+	@DeleteMapping("/user/delete/{user}")
+	public List<User> deleteUser(@RequestBody User user){
+		 userDetailsService.delete(user);
+		 return userDetailsService.loadUsers();
+	}
+	
+	@DeleteMapping("/user/{id}/delete")
+	public List<User> deleteUser(@RequestBody int id){
+		userDetailsService.deleteById(id);
+		return userDetailsService.loadUsers();
+	}
+	
 //	@RequestMapping(value = "/user/register", method = RequestMethod.POST)
 //	public ResponseEntity<?> saveUser(@RequestBody User user) throws Exception {
 //		return ResponseEntity.ok(userDetailsService.save(user));
 //	}
-//
-//}
+
+}
