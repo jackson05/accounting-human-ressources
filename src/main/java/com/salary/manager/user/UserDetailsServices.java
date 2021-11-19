f817119a2b77263ab3827c560e690f56ad968cd9
jackson05 <jackson5g.bi@gmail.com>
2021-11-19 11:30:08 +0200

    authority added to user details

--- src/main/java/com/salary/manager/user/UserDetailsServices.java
+++ src/main/java/com/salary/manager/user/UserDetailsServices.java
@@ -1,13 +1,19 @@
 package com.salary.manager.user;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.salary.manager.user.role.Role;
 
 @Service
 public class UserDetailsServices implements UserDetailsService {
@@ -24,8 +30,12 @@ public class UserDetailsServices implements UserDetailsService {
 		if (user == null) {
 			throw new UsernameNotFoundException("User not found with username: " + userName);
 		}
-		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-				new ArrayList<>());
+		return new org.springframework
+				.security
+				.core
+				.userdetails
+				.User(user.getUsername(), user.getPassword(),
+						rolesToAuthorities(user.getRoles()));
 	}
 	
 	public User save(User user) {
@@ -38,5 +48,20 @@ public class UserDetailsServices implements UserDetailsService {
 		userRepo.findAll().forEach(list::add);
 		 return list;
 		 
+	}
+	
+	
+	private Collection<? extends GrantedAuthority>rolesToAuthorities(List<Role> roles)
+	{
+		return roles.stream().map(role -> 
+		new SimpleGrantedAuthority(role.getRoleName()))
+				.collect(Collectors.toList());
+	}
+	
+	private List<Role>authorities;
+	
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return rolesToAuthorities(authorities);
 	}
 }
