d7c4f9c1c30a62eb7fe03188cb4591a6847659cd
jackson05 <jackson5g.bi@gmail.com>
2021-11-20 21:34:13 +0200

    unused imports removed

--- src/main/java/com/salary/manager/user/UserRepository.java
+++ src/main/java/com/salary/manager/user/UserRepository.java
@@ -3,7 +3,6 @@ package com.salary.manager.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//import com.salary.manager.test.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer>
