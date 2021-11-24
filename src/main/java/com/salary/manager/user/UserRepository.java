45043d54c64a272f012fdf56b35473a73ea3f0c6
jackson05 <jackson5g.bi@gmail.com>
2021-11-24 22:07:57 +0200

    custom query

--- src/main/java/com/salary/manager/user/UserRepository.java
+++ src/main/java/com/salary/manager/user/UserRepository.java
@@ -1,6 +1,8 @@
 package com.salary.manager.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -8,5 +10,8 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User,Integer>
 {
 	User findByUsername(String name);
-
+	
+	@Modifying
+	@Query(value="insert into user_roles(role_id,user_id) values(?,?)",nativeQuery = true)
+	public void saveUserRole(int roleId,int userId );
 }
