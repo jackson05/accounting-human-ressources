cd0c51e9447f84dec3360dfd8a03b48f4415bfe5
jackson05 <jackson5g.bi@gmail.com>
2021-11-24 20:47:10 +0200

    mapping to role object changed

--- src/main/java/com/salary/manager/user/User.java
+++ src/main/java/com/salary/manager/user/User.java
@@ -10,8 +10,6 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import com.salary.manager.user.role.Role;
@@ -31,22 +29,7 @@ public class User {
 	private String lastAcces;
 	private String expiredDate;
 	
-//	@ManyToMany(fetch =FetchType.EAGER ,mappedBy= "user",cascade = CascadeType.ALL)
-	@ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
-	@JoinTable(
-			name="user_roles",
-			joinColumns = @JoinColumn(
-					name="role_id"
-					,
-					referencedColumnName = "id"
-					),
-			inverseJoinColumns = @JoinColumn(
-					name="user_id"
-					,
-					referencedColumnName="id"
-					)
-			
-			)
+	@ManyToMany(fetch =FetchType.EAGER ,mappedBy= "user",cascade = CascadeType.ALL)
 	private Set<Role> roles=new HashSet<>();
 	
 	public int getIdUser() {
