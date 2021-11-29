1bf05558f827e29540f48d5036d574b9cb271b53
jackson05 <jackson5g.bi@gmail.com>
2021-11-29 19:39:41 +0200

    fixed issue and bad import

--- src/main/java/com/salary/manager/user/UserController.java
+++ src/main/java/com/salary/manager/user/UserController.java
@@ -1,32 +1,14 @@
 
   package com.salary.manager.user;
-  
-  import java.util.List; 
-  import org.springframework.beans.factory.annotation.Autowired; 
-  import org.springframework.web.bind.annotation.DeleteMapping; 
-  import org.springframework.web.bind.annotation.GetMapping; 
-  import org.springframework.web.bind.annotation.PathVariable; 
-  import org.springframework.web.bind.annotation.RestController;
-  
-  @RestController public class UserController {
-  
-  // @Autowired // private AuthenticationManager authenticationManager;
-  
-  @Autowired 
-  private UserService userService;
-  
-  @GetMapping("/hello") public String hello() {
-  
-  return "hello world"; }
 
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
