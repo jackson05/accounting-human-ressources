bafb1ed194faaac824ef9025607e72be668c5948
jackson05 <jackson5g.bi@gmail.com>
2021-11-29 11:22:07 +0200

    security temporary removed

--- src/main/java/com/salary/manager/authentication/AuthenticationController.java
+++ src/main/java/com/salary/manager/authentication/AuthenticationController.java
@@ -1,62 +1,57 @@
-package com.salary.manager.authentication;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.salary.manager.security.jwt.model.AuthenticationRequest;
-import com.salary.manager.security.jwt.model.AuthenticationResponse;
-import com.salary.manager.security.jwt.util.JwtTokenUtil;
-import com.salary.manager.user.User;
-import com.salary.manager.user.UserService;
-
-@RestController
-@CrossOrigin
-public class AuthenticationController {
-
-	@Autowired
-	private AuthenticationManager authenticationManager;
-
-	@Autowired
-	private JwtTokenUtil jwtTokenUtil;
-
-	@Autowired
-	private UserService userDetailsServices;
-
-	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
-
-		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
-
-		final UserDetails userDetails = userDetailsServices
-				.loadUserByUsername(authenticationRequest.getUsername());
-
-		final String token = jwtTokenUtil.generateToken(userDetails);
-
-		return ResponseEntity.ok(new AuthenticationResponse(token));
-	}
-	
-	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public ResponseEntity<?> saveUser(@RequestBody User user) throws Exception {
-		return ResponseEntity.ok(userDetailsServices.save(user));
-	}
-
-	private void authenticate(String username, String password) throws Exception {
-		try {
-			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-		} catch (DisabledException e) {
-			throw new Exception("USER_DISABLED", e);
-		} catch (BadCredentialsException e) {
-			throw new Exception("INVALID_CREDENTIALS", e);
-		}
-	}
-}
+/*
+ * package com.salary.manager.authentication;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.http.ResponseEntity; import
+ * org.springframework.security.authentication.AuthenticationManager; import
+ * org.springframework.security.authentication.BadCredentialsException; import
+ * org.springframework.security.authentication.DisabledException; import
+ * org.springframework.security.authentication.
+ * UsernamePasswordAuthenticationToken; import
+ * org.springframework.security.core.userdetails.UserDetails; import
+ * org.springframework.web.bind.annotation.CrossOrigin; import
+ * org.springframework.web.bind.annotation.RequestBody; import
+ * org.springframework.web.bind.annotation.RequestMapping; import
+ * org.springframework.web.bind.annotation.RequestMethod; import
+ * org.springframework.web.bind.annotation.RestController;
+ * 
+ * import com.salary.manager.security.jwt.model.AuthenticationRequest; import
+ * com.salary.manager.security.jwt.model.AuthenticationResponse; import
+ * com.salary.manager.security.jwt.util.JwtTokenUtil; import
+ * com.salary.manager.user.User; import com.salary.manager.user.UserService;
+ * 
+ * @RestController
+ * 
+ * @CrossOrigin public class AuthenticationController {
+ * 
+ * @Autowired private AuthenticationManager authenticationManager;
+ * 
+ * @Autowired private JwtTokenUtil jwtTokenUtil;
+ * 
+ * @Autowired private UserService userDetailsServices;
+ * 
+ * @RequestMapping(value = "/authenticate", method = RequestMethod.POST) public
+ * ResponseEntity<?> createAuthenticationToken(@RequestBody
+ * AuthenticationRequest authenticationRequest) throws Exception {
+ * 
+ * authenticate(authenticationRequest.getUsername(),
+ * authenticationRequest.getPassword());
+ * 
+ * final UserDetails userDetails = userDetailsServices
+ * .loadUserByUsername(authenticationRequest.getUsername());
+ * 
+ * final String token = jwtTokenUtil.generateToken(userDetails);
+ * 
+ * return ResponseEntity.ok(new AuthenticationResponse(token)); }
+ * 
+ * @RequestMapping(value = "/register", method = RequestMethod.POST) public
+ * ResponseEntity<?> saveUser(@RequestBody User user) throws Exception { return
+ * ResponseEntity.ok(userDetailsServices.save(user)); }
+ * 
+ * private void authenticate(String username, String password) throws Exception
+ * { try { authenticationManager.authenticate(new
+ * UsernamePasswordAuthenticationToken(username, password)); } catch
+ * (DisabledException e) { throw new Exception("USER_DISABLED", e); } catch
+ * (BadCredentialsException e) { throw new Exception("INVALID_CREDENTIALS", e);
+ * } } }
+ */
--- src/main/java/com/salary/manager/user/UserService.java
+++ src/main/java/com/salary/manager/user/UserService.java
@@ -1,127 +1,99 @@
+
 package com.salary.manager.user;
 
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.ArrayList; import java.util.Collection; import
+java.util.HashSet; import java.util.List; import java.util.Optional; import
+java.util.Set; import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.Session; import org.hibernate.SessionFactory; 
+import org.hibernate.cfg.Configuration; 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.salary.manager.user.role.Role;
 
-@Service
-public class UserService  implements UserDetailsService {
-	
-	@Autowired
-	private UserRepository userRepo;
-	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
-	
-	@Override
-	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-		User user = userRepo.findByUsername(userName);
-		if (user == null) {
-			throw new UsernameNotFoundException("User not found with username: " + userName);
-		}
-		return new org.springframework
-				.security
-				.core
-				.userdetails
-				.User(user.getUsername(), user.getPassword(),
-						rolesToAuthorities(user.getRoles())
-//						new ArrayList<>()
-						);
-	}
-	
-	
-	
-	
-	private Collection<? extends GrantedAuthority>rolesToAuthorities(Set<Role> roles)
-	{
-		return roles.stream().map(role -> 
-		new SimpleGrantedAuthority(role.getRoleName()))
-				.collect(Collectors.toList());
-	}
-	
-	private Set<Role>authorities;
-	
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		authorities=new HashSet<>();
-		return rolesToAuthorities(authorities);
-	}
-	
-/*
- * SECTION DE GESTION DES CRUD POUR L'UTILISATEUR
- * ===================================================
- */
-	
-	public User save(User user) {
-		user.setPassword(passwordEncoder.encode(user.getPassword()));
-		user.setRoles(user.getRoles());
-		return userRepo.save(user);
-	}
-	
-	public List<User> getUsers() {
-		List<User> list=new ArrayList<>();
-		userRepo.findAll().forEach(list::add);
-		 return list;
-		 
-	}
-	
-	public Optional<User> getUserById(int id) {
-		return	userRepo.findById(id);
-	}
-	
-	public User getUserByUsername(String userName) {
-		return	userRepo.findByUsername(userName);
-	}
-	
-	public void delete(User user) {
-		userRepo.delete(user);
-	}
-	
-	public void deleteById(int id) {
-		userRepo.deleteById(id);
-	}
-	
-	public List<User>loadUsers(){
-		List<User>users=new ArrayList<>();
-		userRepo.findAll().forEach(users::add);
-		return users;
-	}
-	
-	@Transactional
-	public Optional<User> addUserAndRoles(User user) {
-//		SessionFactory sf=  new Configuration().configure().buildSessionFactory();  
-//		Session session = sf.openSession();
-//		session.beginTransaction();
-		for(Role r:user.getRoles()) {
-			userRepo.saveUserRole(r.getId(), user.getIdUser());
-		}
-//		session.getTransaction().commit();
-		
-		return  userRepo.findById(user.getIdUser());
-	}
-	
-	
-	
+@Service 
+public class UserService /* implements UserDetailsService */ {
 
-}
+	@Autowired private UserRepository userRepo;
+
+	/*
+	 * @Autowired private PasswordEncoder passwordEncoder;
+	 * 
+	 * 
+	 * @Override public UserDetails loadUserByUsername(String userName) throws
+	 * UsernameNotFoundException { User user = userRepo.findByUsername(userName); if
+	 * (user == null) { throw new
+	 * UsernameNotFoundException("User not found with username: " + userName); }
+	 * return new org.springframework .security .core .userdetails
+	 * .User(user.getUsername(), user.getPassword(),
+	 * rolesToAuthorities(user.getRoles()) // new ArrayList<>() ); }
+	 * 
+	 * 
+	 * 
+	 * 
+	 * private Collection<? extends GrantedAuthority>rolesToAuthorities(Set<Role>
+	 * roles) { return roles.stream().map(role -> new
+	 * SimpleGrantedAuthority(role.getRoleName())) .collect(Collectors.toList()); }
+	 * 
+	 * private Set<Role>authorities;
+	 * 
+	 * public Collection<? extends GrantedAuthority> getAuthorities() {
+	 * authorities=new HashSet<>(); return rolesToAuthorities(authorities); }
+	 * 
+	 * 
+	 * SECTION DE GESTION DES CRUD POUR L'UTILISATEUR
+	 * ===================================================
+	 * 
+	 * 
+	 * public User save(User user) {
+	 * user.setPassword(passwordEncoder.encode(user.getPassword()));
+	 * user.setRoles(user.getRoles()); return userRepo.save(user); }
+	 */
+
+			public List<User> getUsers() { 
+				List<User> list=new ArrayList<>();
+				userRepo.findAll().forEach(list::add); 
+				return list;
+
+			}
+
+			public Optional<User> getUserById(int id) { return userRepo.findById(id); }
+
+			public User getUserByUsername(String userName) 
+			{ 
+				return
+					userRepo.findByUsername(userName); 
+			}
+
+			public void delete(User user) 
+			{ 
+				userRepo.delete(user); 
+			}
+
+			public void deleteById(int id) 
+			{ 
+				userRepo.deleteById(id);
+			
+			}
+
+			public List<User>loadUsers(){ List<User>users=new ArrayList<>();
+			userRepo.findAll().forEach(users::add); return users; }
+
+		/*
+		 * @Transactional public Optional<User> addUserAndRoles(User user) { //
+		 * SessionFactory sf= new Configuration().configure().buildSessionFactory(); //
+		 * Session session = sf.openSession(); // session.beginTransaction(); for(Role
+		 * r:user.getRoles()) { userRepo.saveUserRole(r.getId(), user.getIdUser()); } //
+		 * session.getTransaction().commit();
+		 * 
+		 * return userRepo.findById(user.getIdUser()); }
+		 */
+
+
+
+
+	}
