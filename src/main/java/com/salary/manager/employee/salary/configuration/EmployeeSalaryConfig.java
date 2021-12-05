c56d1c98981435d70a4e0f4c678a9f1ca7d4eabe
jackson05 <jackson5g.bi@gmail.com>
2021-12-05 20:30:32 +0200

    entity finally defied

--- src/main/java/com/salary/manager/employee/salary/configuration/EmployeeSalaryConfig.java
+++ src/main/java/com/salary/manager/employee/salary/configuration/EmployeeSalaryConfig.java
@@ -1,5 +1,51 @@
 package com.salary.manager.employee.salary.configuration;
 
-public class EmployeeSalaryConfig {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class EmployeeSalaryConfig {
+	
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public int getPourcentage() {
+		return pourcentage;
+	}
+	public void setPourcentage(int pourcentage) {
+		this.pourcentage = pourcentage;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+	private int pourcentage;
+	private int value;
+	private Date createdDate;
+	private Date modifiedDate;
+	
 }
