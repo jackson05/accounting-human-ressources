44b1f95365ebd0b3cff02bbb4be1ad628ae345b0
jackson05 <jackson5g.bi@gmail.com>
2021-12-05 19:16:07 +0200

    methods for salary configuration created

--- src/main/java/com/salary/manager/employee/salary/conf/SalaryConfigurationService.java
+++ src/main/java/com/salary/manager/employee/salary/conf/SalaryConfigurationService.java
@@ -1,5 +1,32 @@
 package com.salary.manager.employee.salary.conf;
 
-public class SalaryConfigurationService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SalaryConfigurationService {
+	
+	@Autowired
+	private SalaryConfigurationRepository salaryConfigurationRepository;
+	
+	public SalaryConfiguration save(SalaryConfiguration salaryConfiguration ) {
+		return salaryConfigurationRepository.save(salaryConfiguration) ;
+	}
+	
+	public void delete(SalaryConfiguration salaryConfiguration) {
+		salaryConfigurationRepository.delete(salaryConfiguration);
+	}
+	
+	public void deleteById(long id) {
+		salaryConfigurationRepository.deleteById(id);
+	}
+	
+	public Optional<SalaryConfiguration> getConfiguration(long id) {
+		return salaryConfigurationRepository.findById(id);
+	}
+	
+	public List<SalaryConfiguration> getSalaryConfigurations(){
+		return salaryConfigurationRepository.findAll();
+	}
 }
