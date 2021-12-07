2e7ffd63fb70cb0e6c5ec68253a7ec44e4895b09
jackson05 <jackson5g.bi@gmail.com>
2021-12-07 12:10:49 +0200

    initial employee salary configuration

---
+++ src/main/java/com/salary/manager/employee/salary/configuration/EmployeeSalaryConfigRepository.java
@@ -0,0 +1,7 @@
+package com.salary.manager.employee.salary.configuration;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeSalaryConfigRepository extends JpaRepository<EmployeeSalaryConfig, Long> {
+
+}
