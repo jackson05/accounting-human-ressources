ce59cffa23d8cea26d3761b09b0ac79c600d88aa
jackson05 <jackson5g.bi@gmail.com>
2021-12-07 13:41:42 +0200

    services for first apis

--- src/main/java/com/salary/manager/employee/salary/configuration/EmployeeSalaryConfigService.java
+++ src/main/java/com/salary/manager/employee/salary/configuration/EmployeeSalaryConfigService.java
@@ -1,5 +1,28 @@
 package com.salary.manager.employee.salary.configuration;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeSalaryConfigService {
+	
+	@Autowired
+	private EmployeeSalaryConfigRepository employeeSalaryConfigRep;
+	
+	public EmployeeSalaryConfig save(EmployeeSalaryConfig employeeSalaryConfig) {
+		return employeeSalaryConfigRep.save(employeeSalaryConfig);
+	}
+	
+	public List<EmployeeSalaryConfig> loadEmployeeSalaryConfig() {
+		return employeeSalaryConfigRep.findAll();
+	}
+	
+//	public EmployeeSalaryConfig makeStoped(EmployeeSalaryConfig employeeSalaryConfig) {
+//		return employeeSalaryConfig;
+//	}
+	
+	
 
 }
