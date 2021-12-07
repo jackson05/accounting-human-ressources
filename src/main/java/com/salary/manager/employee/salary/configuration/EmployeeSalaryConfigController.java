f3f1751258ef318852792b72a9f2099b45896489
jackson05 <jackson5g.bi@gmail.com>
2021-12-07 13:40:57 +0200

    production of first apis

--- src/main/java/com/salary/manager/employee/salary/configuration/EmployeeSalaryConfigController.java
+++ src/main/java/com/salary/manager/employee/salary/configuration/EmployeeSalaryConfigController.java
@@ -1,8 +1,30 @@
 package com.salary.manager.employee.salary.configuration;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeSalaryConfigController {
+	
+	@Autowired
+	private EmployeeSalaryConfigService employeeSalaryConfigService;
+	
+	@PostMapping("/salary/configuration")
+	public EmployeeSalaryConfig 
+		saveEmployeeSalaryConfig(@RequestBody EmployeeSalaryConfig employeeSalaryConfig) {
+		return employeeSalaryConfigService.save(employeeSalaryConfig);
+	}
+	
+	@GetMapping("/salary/configuration")
+	public List<EmployeeSalaryConfig> loadEmployeeSalaryConfig(){
+		return employeeSalaryConfigService.loadEmployeeSalaryConfig();
+	}
+	
+	
 
 }
