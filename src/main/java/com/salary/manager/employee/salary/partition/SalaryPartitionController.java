7837d0f1e8fdf573ebe27487a93d6d7c1b1c2d5d
jackson05 <jackson5g.bi@gmail.com>
2021-12-09 21:25:28 +0200

    rename controller

--- src/main/java/com/salary/manager/employee/salary/partition/SalaryPartitionController.java
+++ src/main/java/com/salary/manager/employee/salary/partition/SalaryPartitionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SalaryConfigurationController {
+public class SalaryPartitionController {
 
 	@Autowired
 	private SalaryPartitionService salaryPartitionservice;
