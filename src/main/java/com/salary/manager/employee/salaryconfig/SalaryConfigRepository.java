799bd785141cdb1955f506168d405da1d66642f4
jackson05 <jackson5g.bi@gmail.com>
2021-12-11 23:59:58 +0200

    new custom method in repository that allow us to find salary configuration by status

--- src/main/java/com/salary/manager/employee/salaryconfig/SalaryConfigRepository.java
+++ src/main/java/com/salary/manager/employee/salaryconfig/SalaryConfigRepository.java
@@ -1,7 +1,12 @@
 package com.salary.manager.employee.salaryconfig;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SalaryConfigRepository extends JpaRepository<SalaryConfig, Long> {
 
+//	@Query("UPDATE")
+	public List<SalaryConfig> findByStoped(boolean stoped);
+//	public SalaryConfig findBySalaryConfig(SalaryConfig salaryConfig);
 }
