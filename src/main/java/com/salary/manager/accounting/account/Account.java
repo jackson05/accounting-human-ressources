ea21a3951b6bcaf8044a7d03b47da47c4083e676
jackson05 <jackson5g.bi@gmail.com>
2022-07-17 21:34:58 +0200

    initial commit for accounting

---
+++ src/main/java/com/salary/manager/accounting/account/Account.java
@@ -0,0 +1,14 @@
+package com.salary.manager.accounting.account;
+import java.util.Date;
+
+public class Account {
+	
+	private int accountId;
+	private String number;
+	private String accountName;
+	private String accountDescription;
+	private Date createdDate;
+	private Date updatedDate;
+	private boolean isDeleteds;
+
+}
